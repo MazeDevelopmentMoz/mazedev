7d183991077e874cbc77c1f38e9a2cbc7a33eb6a
Maze Dev <maze.of.development@gmail.com>
2021-11-02 07:51:00 +0200

    2021_11_02

--- js/appscript.js
+++ js/appscript.js
@@ -6,22 +6,27 @@ var dark = document.getElementById("darktheme")
 if (localStorage.getItem("theme") == null) {
 
     localStorage.setItem("theme", "light")
+    dark.classList.remove("fa-sun")
+    dark.classList.add("fa-moon")
 }
-
 let localData = localStorage.getItem("theme")
+dark.classList.remove("fa-sun")
+dark.classList.add("fa-moon")
 
 if (localData == "dark") {
-    dark.src = "img/sun.png"
+    dark.classList.remove("fa-moon")
+    dark.classList.add("fa-sun")
     document.body.classList.add("dark-theme")
+
 } else if (localData == "light") {
-    dark.src = "img/moon.png"
+    dark.classList.remove("fa-sun")
+    dark.classList.add("fa-moon")
     document.body.classList.remove("dark-theme")
 }
 
 dark.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        dark.src = "img/sun.png"
         dark.classList.remove("fa-moon")
         dark.classList.add("fa-sun")
         localStorage.setItem("theme", "dark")
