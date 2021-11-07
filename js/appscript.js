c11e7f3ef27e2a9fb8235e5c3b5fd124d2e0ef55
Klayton258 <klayton0304massango@gmail.com>
2021-11-07 15:09:29 +0200

    2021_11_07

--- js/appscript.js
+++ js/appscript.js
@@ -9,28 +9,36 @@ if (localStorage.getItem("theme") == null) {
     dark.classList.remove("fa-sun")
     dark.classList.add("fa-moon")
 }
+
 let localData = localStorage.getItem("theme")
 dark.classList.remove("fa-sun")
 dark.classList.add("fa-moon")
 
 if (localData == "dark") {
+
     dark.classList.remove("fa-moon")
     dark.classList.add("fa-sun")
     document.body.classList.add("dark-theme")
 
 } else if (localData == "light") {
+
     dark.classList.remove("fa-sun")
     dark.classList.add("fa-moon")
     document.body.classList.remove("dark-theme")
 }
 
 dark.onclick = function () {
+
     document.body.classList.toggle("dark-theme");
+
     if (document.body.classList.contains("dark-theme")) {
+
         dark.classList.remove("fa-moon")
         dark.classList.add("fa-sun")
         localStorage.setItem("theme", "dark")
+
     } else {
+
         dark.classList.remove("fa-sun")
         dark.classList.add("fa-moon")
         localStorage.setItem("theme", "light")
