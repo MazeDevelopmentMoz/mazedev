0e8362006e4fd65d059a76fda38fccd65c41408f
Klayton258 <klayton0304massango@gmail.com>
2021-11-09 07:10:18 +0200

    2021_11_09

--- js/appscript.js
+++ js/appscript.js
@@ -2,12 +2,14 @@
 //?========================Dark Mode=====================================
 
 var dark = document.getElementById("darktheme")
+var brand = document.getElementById("brand")
 
 if (localStorage.getItem("theme") == null) {
 
     localStorage.setItem("theme", "light")
     dark.classList.remove("fa-sun")
     dark.classList.add("fa-moon")
+    brand.src = "img/brand1.png"
 }
 
 let localData = localStorage.getItem("theme")
@@ -16,6 +18,7 @@ dark.classList.add("fa-moon")
 
 if (localData == "dark") {
 
+    brand.src = "img/brand.png"
     dark.classList.remove("fa-moon")
     dark.classList.add("fa-sun")
     document.body.classList.add("dark-theme")
@@ -32,13 +35,13 @@ dark.onclick = function () {
     document.body.classList.toggle("dark-theme");
 
     if (document.body.classList.contains("dark-theme")) {
-
+        brand.src = "img/brand.png"
         dark.classList.remove("fa-moon")
         dark.classList.add("fa-sun")
         localStorage.setItem("theme", "dark")
 
     } else {
-
+        brand.src = "img/brand2.png"
         dark.classList.remove("fa-sun")
         dark.classList.add("fa-moon")
         localStorage.setItem("theme", "light")
@@ -48,5 +51,6 @@ dark.onclick = function () {
 //?=====================loader==========================================
 setTimeout(function () {
     $(".loader").fadeToggle();
+    $(".page").fadeIn(1500);
 }, 1500)
 //?=====================================================================
